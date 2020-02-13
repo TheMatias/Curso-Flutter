daed29322f88e0c48cad589c3e8dbd0ce3733108
Matias <matiasjose547@gmail.com>
2020-02-13 17:22:56 -0300

    add botal e texto de exibicao

--- calcula_imc/lib/main.dart
+++ calcula_imc/lib/main.dart
@@ -1,11 +1,10 @@
 import 'package:flutter/material.dart';
 
-main(){
+main() {
   runApp(MaterialApp(
-    home: Home(),  
+    home: Home(),
   ));
 }
-
 
 class Home extends StatefulWidget {
   @override
@@ -23,34 +22,49 @@ class _HomeState extends State<Home> {
         actions: <Widget>[
           IconButton(
             icon: Icon(Icons.refresh),
-            onPressed: (){},
+            onPressed: () {},
           )
         ],
       ),
-      body: Column(
+      body: SingleChildScrollView(
+        // padding: EdgeInsets.all(10),
+        padding: EdgeInsets.fromLTRB(50, 0,50,10),
+        child: Column(
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: <Widget>[
           Icon(Icons.person, size: 120, color: Colors.green),
           TextField(
-            keyboardType: TextInputType.numberWithOptions(decimal: true),
-            decoration: InputDecoration(
-              labelText: 'Altura (cm)',
-              labelStyle: TextStyle(color: Colors.green)),
-            textAlign: TextAlign.left,
-            style: TextStyle(color: Colors.green,fontSize: 25)),
+              keyboardType: TextInputType.numberWithOptions(decimal: true),
+              decoration: InputDecoration(
+                  labelText: 'Altura (cm)',
+                  labelStyle: TextStyle(color: Colors.green)),
+              textAlign: TextAlign.left,
+              style: TextStyle(color: Colors.green, fontSize: 25)),
           TextField(
-            keyboardType: TextInputType.numberWithOptions(decimal: true),
-            decoration: InputDecoration(
-              labelText: 'Peso (kg)',
-              labelStyle: TextStyle(color: Colors.green)),
-            textAlign: TextAlign.left,
-            style: TextStyle(color: Colors.green,fontSize: 25)),
-          RaisedButton(
-            onPressed: (){},
-            child: Text('Calcular',style: TextStyle(color: Colors.white, fontSize: 25)),
-            )
+              keyboardType: TextInputType.numberWithOptions(decimal: true),
+              decoration: InputDecoration(
+                  labelText: 'Peso (kg)',
+                  labelStyle: TextStyle(color: Colors.green)),
+              textAlign: TextAlign.left,
+              style: TextStyle(color: Colors.green, fontSize: 25)),
+          Container(
+            height: 50,
+            child: RaisedButton(
+              onPressed: () {},
+              child: Text('Calcular',
+                  style: TextStyle(color: Colors.white, fontSize: 25)),
+              color: Colors.green,
+            ),
+          ),
+          Padding(padding: EdgeInsets.fromLTRB(0,10,0,0)),
+          Text(
+            'Info',
+            textAlign: TextAlign.center,
+            style: TextStyle(color: Colors.green, fontSize: 50,fontWeight: FontWeight.bold),
+          )
         ],
       ),
+      )
     );
   }
 }
